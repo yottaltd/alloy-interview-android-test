ab9837b7854e9e469ddf4a52af627f23a28038c4
Terry Ricks <terry.ricks@yotta.co.uk>
2022-05-06 13:01:49 +0100

    Initial commit

---
+++ app/src/main/java/uk/co/alloy/android/interview/alloyinterviewandroid/MainActivity.kt
@@ -0,0 +1,11 @@
+package uk.co.alloy.android.interview.alloyinterviewandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
